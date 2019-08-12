e98829cc700f400ceaa91bc1148e86d32e092c9e
Toms <tomas.manuel.vazquez@gmail.com>
2019-08-12 20:00:41 -0300

    view Admin v 0.14

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Category;
+use App\Subcategory;
+use App\Product;
+use App\Image;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+      $products = Product::all();
+      $categories = Category::all();
+      $subcategories = Subcategory::all();
+      $images = Image::all();
+      
+      return view('admin',compact('products','categories','subcategories','images'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,4 @@ Route::get('/products', 'ProductController@index')->name('index');
 Route::get('/products/{id}', 'ProductController@show')->name('show');
 
 Route::get('/profile', 'UserController@show')->name('profile');
+Route::get('/admin', 'AdminController@index');
