e9a8af4d647be610d2ee67b93d5be9f0c385f4e5
Gabriela <gabriela.revale@gmail.com>
2019-08-22 15:33:35 -0300

    v.0.29 Avatar default. Agrego Select países y Select Provincias con JS

---
+++ public/js/provinces.js
@@ -0,0 +1,21 @@
+var selectProvinces = document.querySelector('select[name=province]');
+
+
+fetch('https://dev.digitalhouse.com/api/getProvincias')
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (provinces) {
+    console.log(provinces);
+    for (province of provinces['data']) {
+      var option = document.createElement('option');
+      var optionText = document.createTextNode(province.state);
+      option.append(optionText);
+      selectProvinces.append(option);
+    }
+  })
+
+  .catch(function(error) {
+    console.log(error);
+  })
