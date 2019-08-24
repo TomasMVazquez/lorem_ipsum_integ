07f605db8c15d5f51f73f4fc5a75d3cbbfbae2cb
Toms <tomas.manuel.vazquez@gmail.com>
2019-08-24 15:41:20 -0300

    fav added if not login v 0.46

--- public/js/favorite.js
+++ public/js/favorite.js
@@ -1,3 +1,5 @@
+let userLog = document.querySelector('#navbardropLogin');
+
 // obtenemos la cabecera CSRF
 let header = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
@@ -18,36 +20,41 @@ arrayAllForms.forEach(function(form){
   form.addEventListener('submit', function (e) {
     e.preventDefault(); //prevengo que se postee
 
-    //Saco los valores de los elementos y los guardo en un obj literal
-    let dataFav = {
-      product_id: inputsArray[0].value,
-      user_id: inputsArray[1].value,
-    };
+    if (!userLog) {
+      console.log('Logeate');
+      window.location.href = "/login"
+    }else {
+      //Saco los valores de los elementos y los guardo en un obj literal
+      let dataFav = {
+        product_id: inputsArray[0].value,
+        user_id: inputsArray[1].value,
+      };
 
-    //FormData() --> modelo que ya viene dado
-    let dataToSend = new FormData();
+      //FormData() --> modelo que ya viene dado
+      let dataToSend = new FormData();
 
-    //fromJS --> para pegarlo en un JSON
-    dataToSend.append('fromJS', JSON.stringify(dataFav));
-    //Entiendo que el datasend es lo que envia JS en vez del post del form.
-    //Dentro del obj FormData tiene un item que se llama fromJS que le estoy pasando los valores en JSON
-    console.log(dataToSend.get('fromJS'));
-    //hacemos el fetch al url product y le pasamos los methodos el body y el header que le pasamos el token
-    window.fetch('http://localhost:8000/favorito', {
-      method: 'post',
-      body: dataToSend,
-      headers: {'X-CSRF-TOKEN': header} // Para enviar data via fetch
-    })
-      .then(response => response.json())
-      .then(rta => console.log(rta))
-      .catch(error => console.log(error));
+      //fromJS --> para pegarlo en un JSON
+      dataToSend.append('fromJS', JSON.stringify(dataFav));
+      //Entiendo que el datasend es lo que envia JS en vez del post del form.
+      //Dentro del obj FormData tiene un item que se llama fromJS que le estoy pasando los valores en JSON
+      console.log(dataToSend.get('fromJS'));
+      //hacemos el fetch al url product y le pasamos los methodos el body y el header que le pasamos el token
+      window.fetch('http://localhost:8000/favorito', {
+        method: 'post',
+        body: dataToSend,
+        headers: {'X-CSRF-TOKEN': header} // Para enviar data via fetch
+      })
+        .then(response => response.json())
+        .then(rta => console.log(rta))
+        .catch(error => console.log(error));
 
-      //Le cambio la clase al corazon para que este lleno o vacio.
-      if (heart.getAttribute('class') == 'far fa-heart') {
-        heart.setAttribute('class','fas fa-heart')
-      }else {
-        heart.setAttribute('class','far fa-heart')
-      }
+        //Le cambio la clase al corazon para que este lleno o vacio.
+        if (heart.getAttribute('class') == 'far fa-heart') {
+          heart.setAttribute('class','fas fa-heart')
+        }else {
+          heart.setAttribute('class','far fa-heart')
+        }
+    }
 
   });
 });
