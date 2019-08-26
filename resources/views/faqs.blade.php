4b3ce1f3b9cf9a7e48288729fb5f8327f900fb67
Gabriela <gabriela.revale@gmail.com>
2019-08-26 15:38:37 -0300

    v.0.59 Corrección y agregado de FAQs

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -9,7 +9,7 @@
     <h1 class="mt-4">Preguntas Frecuentes</h1>
 
     <p class="m-5">
-      ¿Tenes dudas sobre cómo adquirir nuestros productos? Compartimos 10 FAQs que tenés que saber para comprar los productos de pirulito!. Si tu consulta no se encuentra en este listado, no dudes en contactarnos.
+      ¿Tenés dudas sobre cómo adquirir nuestros productos? Compartimos las FAQs que tenés que saber sobre <strong>Lorem Ipsum Cosmética</strong>. Si tu consulta no se encuentra en este listado, no dudes en contactarnos.
     </p>
 
     <div class="accordion mt-3" id="accordionMobile">
@@ -18,14 +18,14 @@
         <div class="card-header" id="headingOne">
           <h2 class="mb-0">
             <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-              Testean en animales?
+              ¿Testean en animales?
             </button>
           </h2>
         </div>
 
         <div id="collapseOne" class="collapse " aria-labelledby="headingOne" data-parent="#accordionMobile">
           <div class="card-body">
-            No, nuestros productos no son testeados en animales. Hacemos pruebas clínicas y dermatológicas en personas, respaldadas por Dermatólogos y en un laboratorio de referencia internacional.
+            No, nuestros productos no son testeados en animales. Hacemos pruebas clínicas y dermatológicas en personas, respaldadas por especialistas y en un laboratorio de referencia internacional.
           </div>
         </div>
       </div>
@@ -34,13 +34,13 @@
         <div class="card-header" id="headingTwo">
           <h2 class="mb-0">
             <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              ¿Como puedo comprar un producto?
+              ¿Venden productos a través de la web?
             </button>
           </h2>
         </div>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionMobile">
           <div class="card-body">
-            Nosotros solo anunciamos productos de los usuarios registrados en el portal, para comprar un producto debes comunicarte directamente con la persona que publicó el producto.
+            No, <strong>Lorem Ipsum Cosmética</strong> no vende sus productos a través de la página.
           </div>
         </div>
       </div>
@@ -49,13 +49,13 @@
         <div class="card-header" id="headingThree">
           <h2 class="mb-0">
             <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-              ¿Vendemos productos?
+              ¿Sus productos son solo para mujeres?
             </button>
           </h2>
         </div>
         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionMobile">
           <div class="card-body">
-            No, no vende Ninguno de los productos anunciados en la pagina solo somos un portal de clasificados. Para comprar un producto anunciado en la página debes comunicarte directamente con la persona que publicó el anuncio.
+            En <strong>Lorem Ipsum Cosmética</strong> podés encontrar productos para todos, todas, todes, tod@s y todxs.
           </div>
         </div>
       </div>
@@ -64,14 +64,14 @@
         <div class="card-header" id="headingFour">
           <h2 class="mb-0">
             <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseOne">
-              ¿Somos responsable de las transacciones realizadas en la página?
+              ¿Puedo hacerme una cuenta en su página?
             </button>
           </h2>
         </div>
 
         <div id="collapseFour" class="collapse " aria-labelledby="headingFour" data-parent="#accordionMobile">
           <div class="card-body">
-            No somos responsables ni somos intermediario de las transacciones comerciales que se hagan por medio de los usuarios de la página.
+            Por supuesto. Completá el formulario de registro con tus datos y ya tendrás tu cuenta personal.
           </div>
         </div>
       </div>
@@ -86,7 +86,7 @@
         </div>
         <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionMobile">
           <div class="card-body">
-            Si, claro! Ingresá a tu perfil y modifica el formulario. Guardá y listo!
+            Si, claro! Ingresá a tu perfil y modificá el formulario. Guardá y listo!
 
           </div>
         </div>
@@ -96,13 +96,13 @@
         <div class="card-header" id="headingSix">
           <h2 class="mb-0">
             <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseThree">
-              ¿Qué somos?
+              ¿Cómo puedo encontrar el producto que estoy buscando?
             </button>
           </h2>
         </div>
         <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionMobile">
           <div class="card-body">
-            ¡Tiburones!
+            En la página principal vas a encontrar un buscador que te permitirá acceder al producto que estás buscando.
 
           </div>
         </div>
@@ -111,15 +111,14 @@
         <div class="card-header" id="headingSeven">
           <h2 class="mb-0">
             <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="true" aria-controls="collapseOne">
-              Pregunta 7
+              ¿Son responsables de las transacciones realizadas en la página?
             </button>
           </h2>
         </div>
 
         <div id="collapseSeven" class="collapse " aria-labelledby="headingSeven" data-parent="#accordionMobile">
           <div class="card-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            No somos responsables ni somos intermediarios de las transacciones comerciales que realicen les usuaries a través de la página.
           </div>
         </div>
       </div>
@@ -127,15 +126,13 @@
         <div class="card-header" id="headingEight">
           <h2 class="mb-0">
             <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseTwo">
-              Pregunta 8
+              ¿Puedo recibir las novedades de sus productos?
             </button>
           </h2>
         </div>
         <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordionMobile">
           <div class="card-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
+            Sí. Marcá en tu perfil la opción "Quiero recibir noticias!". Mensualmente recibirás un mail con todas las noveades de nuestros productos.
           </div>
         </div>
       </div>
@@ -143,15 +140,13 @@
         <div class="card-header" id="headingNine">
           <h2 class="mb-0">
             <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseThree">
-              Pregunta 9
+              ¿Qué somos?
             </button>
           </h2>
         </div>
         <div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordionMobile">
           <div class="card-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
+            ¡Tiburones!
           </div>
         </div>
       </div>
@@ -165,7 +160,7 @@
           <div class="card-header" id="headingOne">
             <h2 class="mb-0">
               <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                <i class="fas fa-chevron-right mr-2"></i> Testean en animales?
+                <i class="fas fa-chevron-right mr-2"></i> ¿Testean en animales?
               </button>
             </h2>
           </div>
@@ -176,7 +171,7 @@
           <div class="card-header" id="headingTwo">
             <h2 class="mb-0">
               <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                <i class="fas fa-chevron-right mr-2"></i> ¿Como puedo comprar un producto?
+                <i class="fas fa-chevron-right mr-2"></i> ¿Venden productos a través de la web?
               </button>
             </h2>
           </div>
@@ -186,23 +181,24 @@
           <div class="card-header" id="headingThree">
             <h2 class="mb-0">
               <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                <i class="fas fa-chevron-right mr-2"></i> ¿Vendemos productos?
+                <i class="fas fa-chevron-right mr-2"></i>¿Sus productos son solo para mujeres?
+
               </button>
             </h2>
           </div>
 
         </div>
         <div class="card">
-          <div class="card-header" id="headingSix">
+          <div class="card-header" id="headingFour">
             <h2 class="mb-0">
-              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                <i class="fas fa-chevron-right mr-2"></i> ¿Somos responsable de las transacciones realizadas en la página?
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                <i class="fas fa-chevron-right mr-2"></i> ¿Puedo hacerme una cuenta en su página?
               </button>
             </h2>
           </div>
         </div>
         <div class="card">
-          <div class="card-header" id="headingSix">
+          <div class="card-header" id="headingFive">
             <h2 class="mb-0">
               <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
                 <i class="fas fa-chevron-right mr-2"></i> ¿Puedo cambiar mis datos de perfil?
@@ -214,7 +210,7 @@
           <div class="card-header" id="headingSix">
             <h2 class="mb-0">
               <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-                <i class="fas fa-chevron-right mr-2"></i> ¿Qué somos?
+                <i class="fas fa-chevron-right mr-2"></i> ¿Cómo puedo encontrar el producto que estoy buscando?
               </button>
             </h2>
           </div>
@@ -223,7 +219,7 @@
           <div class="card-header" id="headingSeven">
             <h2 class="mb-0">
               <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="true" aria-controls="collapseSeven">
-                <i class="fas fa-chevron-right mr-2"></i> Pregunta 7
+                <i class="fas fa-chevron-right mr-2"></i> ¿Son responsable de las transacciones realizadas en la página?
               </button>
             </h2>
           </div>
@@ -233,7 +229,7 @@
           <div class="card-header" id="headingEight">
             <h2 class="mb-0">
               <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
-                <i class="fas fa-chevron-right mr-2"></i> Pregunta 8
+                <i class="fas fa-chevron-right mr-2"></i> ¿Puedo recibir las novedades de sus productos?
               </button>
             </h2>
           </div>
@@ -243,7 +239,7 @@
           <div class="card-header" id="headingNine">
             <h2 class="mb-0">
               <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
-                <i class="fas fa-chevron-right mr-2"></i> Pregunta 9
+                <i class="fas fa-chevron-right mr-2"></i> ¿Qué somos?
               </button>
             </h2>
           </div>
@@ -253,57 +249,55 @@
       <div class="faqs-rtas">
         <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionDesktop">
           <div class="card-body">
-            No, nuestros productos no son testeados en animales. Hacemos pruebas clínicas y dermatológicas en personas, respaldadas por Dermatólogos y en un laboratorio de referencia internacional.
+            No, nuestros productos no son testeados en animales. Hacemos pruebas clínicas y dermatológicas en personas, respaldadas por especialistas y en un laboratorio de referencia internacional.
           </div>
         </div>
 
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionDesktop">
           <div class="card-body">
-            Nosotros solo anunciamos productos de los usuarios registrados en el portal, para comprar un producto debes comunicarte directamente con la persona que publicó el producto.
+            No, <strong>Lorem Ipsum Cosmética</strong> no vende sus productos a través de la página.
           </div>
         </div>
 
         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionDesktop">
           <div class="card-body">
-            No, no vende Ninguno de los productos anunciados en la pagina solo somos un portal de clasificados. Para comprar un producto anunciado en la página debes comunicarte directamente con la persona que publicó el anuncio.
+            En <strong>Lorem Ipsum Cosmética</strong> podés encontrar productos para todos, todas, todes, tod@s y todxs.
           </div>
         </div>
 
         <div id="collapseFour" class="collapse " aria-labelledby="headingFour" data-parent="#accordionDesktop">
           <div class="card-body">
-            No somos responsables ni somos intermediario de las transacciones comerciales que se hagan por medio de los usuarios de la página.
+            Por supuesto. Completá el formulario de registro con tus datos y ya tendrás tu cuenta personal.
           </div>
         </div>
 
         <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionDesktop">
           <div class="card-body">
-            Si, claro! Ingresá a tu perfil y modifica el formulario. Guardá y listo!
+            Sí, claro! Ingresá a tu perfil y modificá el formulario. Guardá y listo!
           </div>
         </div>
 
         <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionDesktop">
           <div class="card-body">
-            ¡Tiburones!
+            En la página principal vas a encontrar un buscador que te permitirá acceder al producto que estás buscando.
           </div>
         </div>
 
         <div id="collapseSeven" class="collapse " aria-labelledby="headingSeven" data-parent="#accordionDesktop">
           <div class="card-body">
-            ¡Tiburones!
+            No somos responsables ni somos intermediarios de las transacciones comerciales que realicen les usuaries a través de la página.
           </div>
         </div>
 
         <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordionDesktop">
           <div class="card-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Sí. Marcá en tu perfil la opción "Quiero recibir noticias!". Mensualmente recibirás un mail con todas las noveades de nuestros productos.
           </div>
         </div>
 
         <div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordionDesktop">
           <div class="card-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            ¡Tiburones!
           </div>
         </div>
 
