6e2a18a80e214eccc2e716862508a68cb6b8b77f
Gabriela <gabriela.revale@gmail.com>
2019-08-27 20:32:11 -0300

    v.0.68 Modal andando

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -28,11 +28,12 @@
             <div class="img-min">
              @foreach ($product->images as $prodImg)
                <!-- Large modal -->
-            <button type="button" class="" data-toggle="modal" data-target=".bd-example-modal-lg">
+            <button type="button" class="" data-toggle="modal" data-target=".bd-example-modal-lg{{$prodImg->id}}">
               <img src="/storage/items/{{$prodImg->route}}" alt="{{ $product->name }}" style="width:50px;">
             </button>
 
-            <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal fade bd-example-modal-lg{{$prodImg->id}}" tabindex="-1" role="dialog"
+            aria-labelledby="myLargeModalLabel" aria-hidden="true">
               <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                   <img src="/storage/items/{{$prodImg->route}}" alt="{{ $product->name }}" style="width:100%;">
