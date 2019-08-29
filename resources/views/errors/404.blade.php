913a9b2d4f96cad2470696c4159235ff34f226c6
Maria Sol <msolpalma@gmail.com>
2019-08-29 14:39:43 -0300

    v.0.91 Correcciones menores de estilo.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,16 +7,16 @@
 
   </head>
 <body>
-    <div class="container">
-      <div class="row text-center">
-        <div class="col-lg-6 offset-lg-3 col-sm-6 offset-sm-3 col-12 p-3 error-main">
-          <div class="row">
-            <div class="col-lg-8 col-12 col-sm-10 offset-lg-2 offset-sm-1">
-              <h1 class="m-0 title">404</h1>
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-lg-6 col-sm-6  col-12 p-3 error-main">
+        <div class="row">
+          <div class="col-lg-8 col-12 col-sm-10">
+              <h1 class="title">404</h1>
               <h2 class="about">Error!</h2>
               <p>No encontramos lo que estás buscando!.</p>
                 <div class="tomGif">
-                  <img src="/imgs/errorImg/tomGif.gif" alt="gif 404 not found" >
+                  <img src="/imgs/errorImg/tomGif.gif" alt="gif 404 not found" style="width: 100%" >
                 </div>
             </div>
           </div>
